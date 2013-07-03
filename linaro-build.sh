d4a3a6a95e746f8969426a62e6d4f2b52b3fdab3
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2013-07-03 00:14:20 +0000

    initial changes

--- linaro-build.sh
+++ linaro-build.sh
@@ -12,18 +12,10 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # General Public License for more details.
 
-ARG_PREFIX_DIR=/tmp/android-toolchain-eabi
+ARG_PREFIX_DIR=/home/mustaavalkosta/pizzabean/prebuilts/gcc/linux-x86/arm/android-toolchain-eabi-4.8
 ARG_TOOLCHAIN_SRC_DIR=${PWD%/build}
 
-ARG_LINARO_GCC_SRC_DIR=
-ARG_WGET_LINARO_GCC_SRC=
-ARG_BZR_LINARO_GCC_SRC=
-
-ARG_WITH_GCC=
-ARG_WITH_GDB=
 ARG_WITH_SYSROOT=
-
-ARG_APPLY_PATCH=yes
 
 abort() {
   echo $@
@@ -53,116 +45,9 @@ usage() {
   echo
   echo "Valid options (defaults are in brackets)"
   echo "  --prefix=<path>             Specify installation path [/tmp/android-toolchain-eabi]"
-  echo "  --toolchain-src=<path>      Toolchain source directory [`dirname $PWD`]"
-  echo "  --with-gcc=<path>           Specify GCC source (support: directory, bzr, url)"
-  echo "  --with-gdb=<path>           Specify gdb source (support: directory, bzr, url)"
   echo "  --with-sysroot=<path>       Specify SYSROOT directory"
-  echo "  --apply-gcc-patch=<yes|no>  Apply Linaro's extra gcc-patches [yes]"
   echo "  --help                      Print this help message"
   echo
-}
-
-# $1 - package name (gcc, gdb, etc)
-# $2 - value of ARG_WITH_package (lp:*, http://*)
-downloadFromBZR() {
-  local package=$1
-  local MY_LP_LINARO_PKG=$2
-  local version=$3
-
-  local PACKAGE_NAME=`echo $package | tr "[:lower:]" "[:upper:]"`
-  eval "ARG_LINARO_${PACKAGE_NAME}_SRC_DIR=$package-$version"
-
-  dir=${ARG_TOOLCHAIN_SRC_DIR}/$package/$package-$version
-  if [ ! -d "$dir" ]; then
-    info "Use bzr to clone ${MY_LP_LINARO_PKG}"
-    echo bzr clone ${MY_LP_LINARO_PKG} $dir
-    bzr clone ${MY_LP_LINARO_PKG} $dir
-    [ $? -ne 0 ] && error "bzr ${MY_LP_LINARO_PKG} fails."
-  else
-    info "$dir already exists, doing bzr update"
-    (cd $dir; bzr update)
-  fi
-}
-
-# $1 - package name (gcc, gdb, etc)
-# $2 - arg (lp:*, http://*)
-downloadFromHTTP() {
-  local package=$1
-  local url=$2
-  local file=`basename $url`
-
-  local PACKAGE_NAME=`echo $package | tr "[:lower:]" "[:upper:]"`
-
-  info "Use wget to get $file"
-  if [ -f "${ARG_TOOLCHAIN_SRC_DIR}/$package/${file}" ]; then
-    #TODO: Add md5 check
-    info "${file} is already exist, skip download"
-  else
-    wget -c "$url" || error "wget $1 error"
-    # Unpack it so we can apply patches if necessary
-    #TODO: Add md5 check
-    tar -C "${ARG_TOOLCHAIN_SRC_DIR}/$package/" -xf "$file" || error "failed to untar $file"
-  fi
-
-  local src_dir=$(basename $file)
-  src_dir=$(echo $src_dir | sed "s/\(\.tar\.xz\|\.tar\.bz2\|\.tar\.gz\||.txz\|\.tgz\|\.tbz\)//")
-  eval "ARG_LINARO_${PACKAGE_NAME}_SRC_DIR=${src_dir}"
-}
-
-# $1 - value of ARG_WITH_package
-getPackage() {
-  local package
-  local version
-  case $1 in
-    lp:*)
-      package=${1##lp:}
-      version=${package#*-}
-      # Replace / with -
-      version=${version////-}
-      package=${package%%-*}
-      info "Detected package \"$package\" and version \"$version\" for $1 bzr repo"
-      ;;
-    *)
-      package=$(basename $1)
-      version=${package#*-}
-      version=$(echo $version | sed "s/\(\.tar\.bz2\|\.tar\.gz\|\.tgz\|\.tbz\)//")
-      package=${package%%-*}
-      ;;
-  esac
-
-  local PACKAGE_NAME=`echo $package | tr "[:lower:]" "[:upper:]"`
-  # Make sure that package dir exists
-  mkdir -p ${ARG_TOOLCHAIN_SRC_DIR}/$package || error "Cannot create ${ARG_TOOLCHAIN_SRC_DIR}/$package directory"
-
-  case $1 in
-    lp:*) # bzr clone lp:gcc-linaro
-      downloadFromBZR $package $1 $version
-      ;;
-    http://*) # snapshot URL
-      # http://launchpad.net/gcc-linaro/4.5/4.5-2011.04-0/+download/gcc-linaro-4.5-2011.04-0.tar.bz2
-      downloadFromHTTP $package $1
-      ;;
-    *) # local directory
-      [ ! -d "${ARG_TOOLCHAIN_SRC_DIR}/$package/$1" ] && \
-        echo "Directory $ARG_TOOLCHAIN_SRC_DIR/$package/$1 does not exist" && \
-        mkdir -p $ARG_TOOLCHAIN_SRC_DIR/$package && \
-        error "Please extract the $package source into directory $ARG_TOOLCHAIN_SRC_DIR/$package"
-      eval "ARG_LINARO_${PACKAGE_NAME}_SRC_DIR=$1"
-  esac
-
-  # verify version
-  case $package in
-    gcc)
-      # make sure version is greater than 4.5
-      ver=`echo $version | grep -o "^4\.\([5-9]\|[1-9][0-9]\)"`
-      if [ x"$version" = x"" ]; then
-        warn "Cannot detect version for $package-$version, 4.5 is used"
-        version="4.5"
-      fi
-      ;;
-  esac
-
-  eval "ARG_LINARO_${PACKAGE_NAME}_VER=$version"
 }
 
 while [ $# -gt 0 ]; do
@@ -185,9 +70,6 @@ while [ $# -gt 0 ]; do
     --with-sysroot=*)
       ARG_WITH_SYSROOT="${ARG#*=}"
       ;;
-    --apply-gcc-patch=yes | --apply-gcc-patch=no)
-      ARG_APPLY_PATCH="${ARG#*=}"
-      ;;
     --help)
       usage && abort
       ;;
@@ -202,48 +84,11 @@ BUILD_WITH_LOCAL=
 BUILD_HOST=
 BUILD_SYSROOT=
 
-[ -z "${ARG_TOOLCHAIN_SRC_DIR}" ] && [ -x "`pwd`"/../build/configure ] && ARG_TOOLCHAIN_SRC_DIR="`pwd`/.."
-if [ "${ARG_TOOLCHAIN_SRC_DIR}" = "" ] || [ ! -f "${ARG_TOOLCHAIN_SRC_DIR}/build/configure" ] ; then
-  error "--toolchain-src is not set or ${ARG_TOOLCHAIN_SRC_DIR} is not ANDROID_TOOLCHAIN_ROOT"
-fi
-
-if [ x"${ARG_WITH_GCC}" = x"" ]; then
-  error "Must specify --with-gcc to build toolchain"
-fi
-
 if [ ! -z "${ARG_WITH_SYSROOT}" ]; then
   if [ ! -d "${ARG_WITH_SYSROOT}" ]; then
     error "SYSROOT ${ARG_WITH_SYSROOT} not exist"
   fi
   BUILD_SYSROOT="--with-sysroot=${ARG_WITH_SYSROOT}"
-fi
-
-for package in ${!ARG_WITH*}; do
-  if [ x"${!package}" != "x" ]; then
-    getPackage ${!package}
-  fi
-done
-
-if [ "x${ARG_APPLY_PATCH}" = "xyes" ]; then
-  sub_gcc_ver="`echo ${ARG_LINARO_GCC_VER} | grep -o '4\.\([5-9]\|[1-9][0-9]\)'`"
-  note "Will apply patches in toolchain/gcc-patches/${sub_gcc_ver}"
-  cd ${ARG_TOOLCHAIN_SRC_DIR}/gcc/${ARG_LINARO_GCC_SRC_DIR} &&
-  for FILE in `ls ${ARG_TOOLCHAIN_SRC_DIR}/gcc-patches/${sub_gcc_ver} 2>/dev/null` ; do
-    if [ ! -f ${FILE}-patch.log ]; then
-      note "Apply patch: ${FILE}"
-      git apply ${ARG_TOOLCHAIN_SRC_DIR}/gcc-patches/${sub_gcc_ver}/${FILE} 2>&1 | \
-        tee "${FILE}-patch.log"
-    fi
-  done
-  cd -
-fi
-
-if [ x"${ARG_WITH_GCC}" != x"" ]; then
-  BUILD_WITH_GCC="--with-gcc-version=${ARG_LINARO_GCC_VER}"
-fi
-
-if [ x"${ARG_WITH_GDB}" != x"" ]; then
-  BUILD_WITH_GDB="--with-gdb-version=${ARG_LINARO_GDB_VER}"
 fi
 
 if echo "$BUILD_ARCH" | grep -q '64' ; then
@@ -262,18 +107,25 @@ fi
 [ -z "$TARGET" ] && TARGET=arm-linux-androideabi
 
 ${ARG_TOOLCHAIN_SRC_DIR}/build/configure \
-  --prefix=${ARG_PREFIX_DIR} --target=${TARGET} \
+  --prefix=/home/mustaavalkosta/pizzabean/prebuilts/gcc/linux-x86/arm/android-toolchain-eabi-4.8 \
+  --target=${TARGET} \
   --disable-docs --disable-nls \
+  --enable-graphite \
+  --enable-gold=default \
   --host=${BUILD_HOST} --build=${BUILD_HOST} \
   ${BUILD_SYSROOT} \
   \
-  ${BUILD_WITH_GCC} \
-  ${BUILD_WITH_GDB} \
-  --with-binutils-version=${BINUTILS_VERSION-current} \
+  --with-gcc-version=4.8 \
+  --with-gdb-version=linaro-7.6-2013.05 \
+  --with-binutils-version=2.23.2 \
   \
-  --with-gmp-version=${GMP_VERSION-current} \
-  --with-mpfr-version=${MPFR_VERSION-current} \
-  --with-mpc-version=${MPC_VERSION-current} \
+  --with-gmp-version=5.1.2 \
+  --with-mpfr-version=3.1.2 \
+  --with-mpc-version=1.0.1 \
+  --with-cloog-version=0.18.0 \
+  --with-ppl-version=1.0 \
+  --disable-ppl-version-check \
+  --disable-cloog-version-check \
   \
   ${LINARO_BUILD_EXTRA_CONFIGURE_FLAGS}
 
